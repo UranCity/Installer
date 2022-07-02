0aca6e42e239c788126069c58cb89f83aadb957d
ValentinKhmer <valentin_kh@outlook.fr>
2022-07-02 18:14:55 +0200

    Update for 1.1.0

--- index.js
+++ index.js
@@ -6,14 +6,11 @@ const AdmZip = require("adm-zip"); // 29.6KB (10.46KB zipped)
 const bar = require("cli-progress"); // 32.68KB (10.06KB zipped)
 const readline = require("readline"); // in Node
 const superagent = require("superagent"); // 55.66KB (17.25KB zipped)
-const { spawn } = require("child_process"); // No calculed
-
-// Local data
-const toSave = require("./save.json");
-const profile = require("./profile.json");
-
+const { spawn } = require("child_process"); // in Node
+var crypto = require("crypto");
 //* Title Screen
-console.log(
+
+const splash =
 	chalk.hex("#3FFEFE")(`\n\
        █████  █████                              █████████  ███  █████             \n\
       ░░███  ░░███                              ███░░░░░███░░░  ░░███              \n\
@@ -26,74 +23,63 @@ console.log(
                                                                           ███ ░███ \n\
                                                                          ░░██████  \n\
                                                                           ░░░░░░   \n\
-\n`) + ` Développé par ${chalk.hex("#5271FF")("ValentinKhmer")}\n`
-);
+\n`) + ` Développé par ${chalk.hex("#5271FF")("ValentinKhmer")}\n`;
+
+const rl = readline.createInterface(process.stdin, process.stdout);
 
 (async function () {
-	console.log(chalk.bold(" Préparation de l'installation..."));
-
-	const fml = {
-		minecraft: "1.16.5",
-		version: "36.2.20",
-	};
-
-	// Define paths
-	const downloadPath = path.join(process.env.USERPROFILE, "downloads");
-	const urancityPath = path.join(process.env.APPDATA, ".urancity");
-	const urancityTempPath = path.join(process.env.APPDATA, ".uranciy-save");
-
-	const rl = readline.createInterface(process.stdin, process.stdout);
+	console.log(splash + chalk.bold(`\n Préparation de l'installation...`));
 	const wait = async (ms) =>
 		await new Promise((resolve) => setTimeout(resolve, ms ? ms : 1000));
+
+	const api = (
+		await superagent
+			.get(`https://api.urancity.ml/client`)
+			.set("User-Agent", "UranCityInstaller/1.1.0")
+			.retry(0)
+	).body;
+
+	// Define paths
+	const paths = {
+		download: path.join(process.env.USERPROFILE, "downloads"), // folder
+		urancity: path.join(process.env.APPDATA, ".urancity"), // folder
+		urancityTemp: path.join(process.env.APPDATA, ".uranciy-save"), // folder
+		forge: path.join(
+			process.env.APPDATA,
+			".minecraft",
+			"versions",
+			`${api.info.minecraft}-forge-${api.info.forge}`,
+			`${api.info.minecraft}-forge-${api.info.forge}.jar` // file
+		),
+	};
 
 	// Check if last version is installed
 	try {
 		if (
-			// Get the last version
 			(
 				await superagent.get(
 					"https://raw.githubusercontent.com/UranCity/Client/main/VERSION"
 				)
-			).text ===
-			// Get the current version
-			(() => {
-				if (fs.existsSync(path.join(urancityPath, "VERSION"))) {
-					return fs.readFileSync(path.join(urancityPath, "VERSION"));
-				}
-				return null;
-			})()
-		) {
-			error("Vous possédez déjà la dernière version du client !");
-		}
+			).text === fs.existsSync(path.join(paths.urancity, "VERSION"))
+				? fs.readFileSync(path.join(paths.urancity, "VERSION"))
+				: undefined
+		)
+			await error("Vous possédez déjà la dernière version du client !");
 	} catch (err) {
-		error(err);
+		await error(err);
 	}
 
-	const size =
-		Math.round(
-			await superagent
-				.get("https://api.github.com/repos/UranCity/Client")
-				.set("User-Agent", "SuperAgent 6.1.0")
-				.then((res) => res.body.size / 10)
-		) / 100;
-
-	try {
+		try {
 		await new Promise((resolve) => {
 			rl.question(
-				` Taille du client: ${size}MB` +
-					`\n Espace nécessaire*: ~${size * 2}MB` +
-					`\n Espace disponible: N/A` +
-					chalk.italic(
-						`\n\n *Espace nécessaire pendant l'installation et non occupé par le client !`
-					) +
-					`\n Continuer l'installation ? [Entrée]`,
+				`\n Confirmez-vous l'installation ? [Entrée]`,
 				() => {
 					resolve();
 				}
 			);
 		});
 	} catch (error) {
-		error(error);
+		await error(error);
 	}
 
 	const cliBar = new bar.SingleBar({
@@ -103,247 +89,330 @@ console.log(
 		cloneFiles: false,
 		barCompleteChar: "\u2588",
 		barIncompleteChar: "\u2591",
-		hideCursor: true,
+		hideCursor: false,
 		forceRedraw: true,
 	});
 
-	// Check if the correct forge version is installed
-	let forge = true;
-	if (
-		!fs.existsSync(
-			path.join(
-				process.env.APPDATA,
-				".minecraft",
-				"versions",
-				`${fml.minecraft}-forge-${fml.version}`,
-				`${fml.minecraft}-forge-${fml.version}.jar`
-			)
-		)
-	)
-		forge = false;
-
-	console.log(chalk.bold("\n Téléchargement des fichiers..."));
-	cliBar.start(100, 0);
-
-	const fileName = "UranCity_Client_main.zip";
-
-	try {
-		// TODO: Download client's zip file
-		await new Promise((resolve, reject) => {
-			superagent
-				.get("https://github.com/UranCity/Client/archive/refs/heads/main.zip")
-				.on("progress", (state) => {
-					cliBar.update(forge ? state.percent : state.percent / 2);
-				})
-				.on("error", (err) => {
-					reject(err);
-				})
-				.pipe(fs.createWriteStream(path.join(downloadPath, fileName)))
-				.on("finish", () => {
-					resolve();
-				});
-		});
-
-		if (forge) {
-			await wait();
-			cliBar.update(50);
-
-			await new Promise((resolve, reject) => {
-				superagent
-					.get(
-						`https://maven.minecraftforge.net/net/minecraftforge/forge/${fml.minecraft}-${fml.version}/forge-${fml.minecraft}-${fml.version}-installer.jar`
-					)
-					.on("progress", (state) => {
-						cliBar.update(50 + state.percent / 2);
-					})
-					.on("error", (err) => {
-						reject(err);
-					})
-					.pipe(
-						fs.createWriteStream(
-							path.join(
-								downloadPath,
-								`forge-${fml.minecraft}-${fml.version}-installer.jar`
-							)
-						)
-					)
-					.on("finish", () => {
-						resolve();
-					});
-			});
-		}
-	} catch (err) {
-		error(err);
-	}
-
-	await wait();
-	cliBar.update(100);
-
-	// TODO: Save personals files
-
-	if (fs.existsSync(urancityPath)) {
-		console.log(chalk.bold("\n Sauvegarde des fichiers personnels..."));
-
-		cliBar.start(100, 0);
-		try {
-			if (!fs.existsSync(urancityTempPath)) {
-				fs.mkdir(urancityTempPath, (err) => {
-					if (err) return error(err);
-				});
-			}
-
-			for (let e = 0; e < toSave.length; e++) {
-				if (fs.existsSync(path.join(urancityPath, toSave[e]))) {
-					fs.renameSync(
-						path.join(urancityPath, toSave[e]),
-						path.join(urancityTempPath, toSave[e])
-					);
-				}
-				cliBar.update(Math.round((e / toSave.length) * 100));
-			}
-
-			await wait();
-
-			fs.rmdirSync(urancityPath, { recursive: true, force: true });
-			cliBar.update(100);
-
-			await wait();
-		} catch (err) {
-			return error(err);
-		}
-	}
-
-	console.log(chalk.bold("\n Extraction des fichiers..."));
-	cliBar.start(100, 0);
-
-	const zip = AdmZip(path.join(downloadPath, fileName));
-	zip.extractAllTo(process.env.APPDATA);
-
-	cliBar.update(50);
-	await wait();
-
-	fs.renameSync(path.join(process.env.APPDATA, "Client-main"), urancityPath);
-
-	cliBar.update(100);
-	await wait();
-
-	if (!forge) {
-		try {
-			console.log(chalk.bold("\n Installation de Forge..."));
-			cliBar.start(100, 0);
-			while (
-				fs.existsSync(
-					path.join(
-						process.env.APPDATA,
-						".minecraft",
-						"versions",
-						`${fml.minecraft}-forge-${fml.version}`,
-						`${fml.minecraft}-forge-${fml.version}.jar`
-					)
-				)
-			) {
+	// if "confition" is true, then execute the "then" function
+	const step = [
+		{
+			name: "Téléchargement des données de jeu",
+			condition: true,
+			run: async () =>
 				await new Promise((resolve, reject) => {
-					spawn(`java`, [
-						"-jar",
-						path.join(
-							downloadPath,
-							`forge-${fml.minecraft}-${fml.version}-installer.jar`
-						),
-					])
+					superagent
+						.get(
+							"https://github.com/UranCity/Client/archive/refs/heads/main.zip"
+						)
+						.on("progress", (state) => {
+							cliBar.update(state.percent);
+						})
 						.on("error", (err) => {
 							reject(err);
 						})
-						.on("close", () => {
+						.pipe(
+							fs.createWriteStream(
+								path.join(paths.download, api.info.clientFilename)
+							)
+						)
+						.on("finish", () => {
+							resolve();
+						});
+				}),
+		},
+		{
+			name: "Téléchargement de Forge",
+			condition: !fs.existsSync(paths.forge),
+			run: async () =>
+				await new Promise((resolve, reject) => {
+					superagent
+						.get(
+							`https://maven.minecraftforge.net/net/minecraftforge/forge/${api.info.minecraft}-${api.info.forge}/forge-${api.info.minecraft}-${api.info.forge}-installer.jar`
+						)
+						.on("progress", (state) => {
+							cliBar.update(state.percent);
+						})
+						.on("error", (err) => {
+							reject(err);
+						})
+						.pipe(
+							fs.createWriteStream(
+								path.join(
+									paths.download,
+									`forge-${api.info.minecraft}-${api.info.forge}-installer.jar`
+								)
+							)
+						)
+						.on("finish", () => {
+							resolve();
+						});
+				}),
+		},
+		{
+			name: "Sauvegarde des fichiers personnels",
+			condition: fs.existsSync(paths.urancity),
+			run: async () => {
+				if (!fs.existsSync(paths.urancityTemp)) {
+					fs.mkdir(paths.urancityTemp, async (err) => {
+						if (err) return await error(err);
+					});
+				}
+
+				for (let e = 0; e < api.save.length; e++) {
+					if (fs.existsSync(path.join(paths.urancity, api.save[e]))) {
+						fs.renameSync(
+							path.join(paths.urancity, api.save[e]),
+							path.join(paths.urancityTemp, api.save[e])
+						);
+					}
+					cliBar.update(Math.round((e / api.save.length) * 100));
+				}
+
+				await wait();
+
+				fs.rmdirSync(paths.urancity, { recursive: true, force: true });
+			},
+		},
+		{
+			name: "Extraction des fichiers",
+			condition: true,
+			run: async () => {
+				const zip = new AdmZip(
+					path.join(paths.download, api.info.clientFilename)
+				);
+				zip.extractAllTo(process.env.APPDATA);
+
+				cliBar.update(95);
+				await wait();
+
+				fs.renameSync(
+					path.join(process.env.APPDATA, "Installer-main"),
+					paths.urancity
+				);
+			},
+		},
+		{
+			name: "Téléchargement des mods",
+			condition: true,
+			run: async () => {
+				fs.mkdirSync(path.join(paths.urancity, "mods"));
+
+				for (let m = 0; m < api.mods.length; m++) {
+					const modId = [
+						api.mods[m].versionId.slice(0, 4).replace(/^0+/, ""),
+						api.mods[m].versionId.slice(-3).replace(/^0+/, ""),
+					].join("/");
+
+					await new Promise((resolve, reject) => {
+						superagent
+							.get(
+								`https://media.forgecdn.net/files/${modId}/${api.mods[m].filename}.jar`
+							)
+							.on("progress", (state) => {
+								cliBar.update(state.percent / 2);
+							})
+							.on("error", (err) => {
+								reject(err);
+							})
+							.pipe(
+								fs.createWriteStream(
+									path.join(
+										paths.urancity,
+										"mods",
+										`${api.mods[m].filename}.jar`
+									)
+								)
+							)
+							.on("finish", () => {
+								resolve();
+							});
+					});
+
+					const modHash = crypto
+						.createHash("md5")
+						.update(
+							fs.readFileSync(
+								path.join(paths.urancity, "mods", `${api.mods[m].filename}.jar`)
+							)
+						)
+						.digest("hex");
+
+					if (modHash !== api.mods[m].hash)
+						return await error(
+							`Le hash du mod ${api.mods[m].filename} est invalide.`
+						);
+
+					cliBar.update(Math.round((m / api.mods.length) * 100));
+				}
+			},
+		},
+		{
+			name: "Téléchargement d'OptiFine",
+			condition: true,
+			run: async () => {
+				const ofUrl = (
+					await superagent.get(
+						`https://optifine-dl-link.vercel.app/api?mc=${api.info.minecraft}&of=${api.info.optifine}`
+					)
+				).body.url;
+
+				await new Promise((resolve, reject) => {
+					superagent
+						.get(ofUrl)
+						.on("progress", (state) => {
+							cliBar.update(Math.round((state.percent * 99) / 100));
+						})
+						.on("error", (err) => {
+							reject(err);
+						})
+						.pipe(
+							fs.createWriteStream(
+								path.join(
+									paths.urancity,
+									"mods",
+									`OptiFine_${api.info.minecraft}_HD_U_${api.info.optifine}.jar`
+								)
+							)
+						)
+						.on("finish", () => {
 							resolve();
 						});
 				});
+
+				const ofHash = crypto
+					.createHash("md5")
+					.update(
+						fs.readFileSync(
+							path.join(
+								paths.urancity,
+								"mods",
+								`OptiFine_${api.info.minecraft}_HD_U_${api.info.optifine}.jar`
+							)
+						)
+					)
+					.digest("hex");
+				if (ofHash !== "e0717ba6d1674af83036ebc479fc77b4")
+					return await error(`Le hash d'OptiFine est invalide.`);
+			},
+		},
+		{
+			name: "Installation de Forge",
+			condition: !fs.existsSync(paths.forge),
+			run: async () => {
+				while (fs.existsSync(paths.forge)) {
+					await new Promise((resolve, reject) => {
+						spawn(`java`, [
+							"-jar",
+							path.join(
+								paths.download,
+								`forge-${api.info.minecraft}-${api.info.forge}-installer.jar`
+							),
+						])
+							.on("error", (err) => {
+								reject(err);
+							})
+							.on("close", () => {
+								resolve();
+							});
+					});
+				}
+			},
+		},
+		{
+			name: "Restauration des fichiers personnels",
+			condition: fs.existsSync(paths.urancityTemp),
+			run: async () => {
+				fs.readdirSync(paths.urancityTemp).forEach((file) => {
+					fs.renameSync(
+						path.join(paths.urancityTemp, file),
+						path.join(paths.urancity, file)
+					);
+				});
+				fs.rmdirSync(paths.urancityTemp, { recursive: true, force: true });
+			},
+		},
+		{
+			name: "Installation du profile",
+			condition: true,
+			run: async () => {
+				let existJson = false;
+
+				[
+					"launcher_profiles.json", // Launcher Mojang (old)
+					"launcher_profiles_microsoft_store.json", // Launcher Microsoft (new)
+				].forEach((jsonProfile) => {
+					const theoreticalPath = path.join(
+						process.env.APPDATA,
+						".minecraft",
+						jsonProfile
+					);
+					if (fs.existsSync(theoreticalPath)) {
+						const data = JSON.parse(fs.readFileSync(theoreticalPath));
+						const profileObj = api.profile;
+
+						profileObj.gameDir = paths.urancity;
+						data.profiles.urancity = profileObj;
+
+						fs.writeFileSync(theoreticalPath, JSON.stringify(data, null, 2));
+						existJson = true;
+					}
+				});
+
+				if (!existJson)
+					await error(
+						" Aucun launcher reconnu...\n Merci de relancer le programme en ayant lancé Minecraft au moins 1 fois sur votre machine."
+					);
+			},
+		},
+		{
+			name: "Supression des déchets",
+			condition: fs.existsSync(
+				path.join(paths.download, api.info.clientFilename)
+			),
+			run: async () => {
+				fs.unlinkSync(path.join(paths.download, api.info.clientFilename));
+			},
+		},
+	];
+
+	for (let s = 0; s < step.length; s++) {
+		if (step[s].condition) {
+			console.log(splash + chalk.bold(`\n ${step[s].name}...`));
+			cliBar.start(100, 0);
+			try {
+				await step[s].run();
+			} catch (err) {
+				await error(err);
 			}
-		} catch (err) {
-			error(err);
+			await wait();
+			cliBar.update(100);
 		}
-		cliBar.update(100);
-		await wait();
-	}
-
-	if (fs.existsSync(urancityTempPath)) {
-		console.log(chalk.bold("\n Importation de la sauvegarde..."));
-
-		cliBar.start(100, 0);
-		try {
-			await fs.readdirSync(urancityTempPath).forEach((file) => {
-				fs.renameSync(
-					path.join(urancityTempPath, file),
-					path.join(urancityPath, file)
-				);
-			});
-			fs.rmdirSync(urancityTempPath, { recursive: true, force: true });
-		} catch (err) {
-			error(err);
-		}
-		cliBar.update(100);
-	}
-
-	console.log(chalk.bold("\n Installation du profile..."));
-	cliBar.start(100, 0);
-
-	try {
-		let existJson = false;
-
-		const jsonProfiles = [
-			"launcher_profiles.json", // Launcher Mojang (old)
-			"launcher_profiles_microsoft_store.json", // Launcher Microsoft (new)
-		];
-
-		jsonProfiles.forEach((jsonProfile) => {
-			const theoreticalPath = path.join(
-				process.env.APPDATA,
-				".minecraft",
-				jsonProfile
-			);
-			if (fs.existsSync(theoreticalPath)) {
-				const data = JSON.parse(fs.readFileSync(theoreticalPath));
-				const profileObj = profile;
-
-				profileObj.gameDir = urancityPath;
-				data.profiles.urancity = profileObj;
-
-				fs.writeFileSync(theoreticalPath, JSON.stringify(data, null, 2));
-				existJson = true;
-			}
-		});
-		cliBar.update(100);
-
-		if (!existJson)
-			error(
-				" Aucun launcher reconnu...\n Merci de relancer le programme en ayant lancé Minecraft au moins 1 fois sur votre machine."
-			);
-	} catch (err) {
-		error(err);
-	}
-
-	console.log(chalk.bold("\n Supression des déchets..."));
-	if (fs.existsSync(path.join(downloadPath, fileName))) {
-		fs.unlinkSync(path.join(downloadPath, fileName));
 	}
 
 	rl.question(
-		`\n ${chalk.green.bold(
-			"Installation terminée !"
-		)}\n Appuyez sur [Entrée] pour quitter l'installation...`,
+		splash +
+			`\n${chalk.green.bold(
+				"Installation terminée !"
+			)}\n Appuyez sur [Entrée] pour quitter l'installation... `,
 		() => {
 			process.exit();
 		}
 	);
 })();
 
-function error(err) {
-	console.error(chalk.red.bold(`\n\n ${err}\n\n`));
-	console.log(
-		chalk.inverse(
-			"Besoin d'aide ? https://github.com/UranCity/Installer/issues/new/choose"
-		)
-	);
-	rl.question("Appuyez sur [Entrée] pour quitter l'installation...", () => {
-		process.exit();
+const error = async (err) =>
+	await new Promise(async (resolve, reject) => {
+		console.error(chalk.red.bold(`\n\n ${err}\n\n`));
+		console.log(
+			chalk.inverse(
+				"Besoin d'aide ? https://github.com/UranCity/Installer/issues/new/choose"
+			)
+		);
+		await rl.question(
+			"Appuyez sur [Entrée] pour quitter l'installation...",
+			() => {
+				process.exit();
+				resolve();
+			}
+		);
 	});
-}
